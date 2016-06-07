bf03aec5be1cb6629bc80b4990c7e2909041ee54
Zacoist <zachary.tan@hotmail.com>
2016-06-07 13:17:36 -0400

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -4,7 +4,9 @@ import javax.swing.JPanel;
  */
 public class Board {
 	private int clickCount = 0;
-	private String piece;
+	private int tempX;
+	private int tempY;
+	private String pieceID;
 	ChessGame game;
 	
 	//Instantiate 2D array of Pieces 
@@ -15,12 +17,32 @@ public class Board {
 	public Board(ChessGame game) {
 		this.game = game;
 		
+		//Pawns (for both sides)
 		for (int i = 0; i < 8; i++) {
 			board[1][i] = new Pawn('b');
 			board[6][i] = new Pawn('w');
 		}
+		//Black
 		board[0][4] = new King('b');
+		board[0][3] = new Queen('b');
+		board[0][0] = new Rook('b');
+		board[0][7] = new Rook('b');
+		board[0][1] = new Knight('b');
+		board[0][6] = new Knight('b');
+		board[0][2] = new Bishop('b');
+		board[0][5] = new Bishop('b');
 		
+		//White
+		board[7][4] = new King('w');
+		board[7][3] = new Queen('w');
+		board[7][0] = new Rook('w');
+		board[7][7] = new Rook('w');
+		board[7][1] = new Knight('w');
+		board[7][6] = new Knight('w');
+		board[7][2] = new Bishop('w');
+		board[7][5] = new Bishop('w');
+		
+		//White
 	}//End of Board()
 	
 	//METHODS
@@ -31,14 +53,32 @@ public class Board {
 		clickCount++;
 		System.out.println(clickCount);
 			if (clickCount == 1){
-				piece = board[x][y].getName();
+				//Save previous coordinates
+				tempX = x;
+				tempY = y;
+				System.out.println(board[x][y].getName()+": " + tempX + ","+ tempY);
 			}
-			if (clickCount == 2  && piece == board[x][y].getName()){
-				clickCount-=2;
+			//If the user clicks on the same JLabel, it should "deselect"
+			else if (clickCount == 2 && (board[x][y] == board[tempX][tempY])){
+				clickCount = 0;
+				System.out.println("Deselected");
+				//Clear piece ID
+				pieceID = " ";
+				board[x][y] = null;
+			}
+			//If the user clicks two different JLabels, it should identify both Jlabels
+			else if (clickCount == 2 && (board[x][y] != board[tempX][tempY])){
+				tempX = x;
+				tempY = y;
+				System.out.println(board[x][y].getName()+": " + tempX + ","+ tempY);
+				clickCount = 0;
+			}
+			else if (board[x][y] == null && (clickCount == 1 || clickCount == 2)) {
+				clickCount = 0;
+				System.out.println("empty");
 			}
 				
-		/*board[x][y] = null;
-		if (board[x][y] == null) {
+		/*if (board[x][y] == null) {
 			System.out.println("empty");
 		} else {
 			System.out.println(board[x][y].getName() + ": " + board[x][y].getColour());
@@ -51,4 +91,7 @@ public class Board {
 	public Piece[][] getBoard() {
 		return board;
 	}//End of getBoard()
+	
+	
+	
 }
