792dcf4171ad3125dacaf91243ea00167edd7bb9
Zacoist <zachary.tan@hotmail.com>
2016-06-09 13:23:45 -0400

    Added the moving method

--- src/Knight.java
+++ src/Knight.java
@@ -19,10 +19,76 @@ public class Knight extends Piece {
 		// TODO Auto-generated method stub
 
 	}
-
+	
 	@Override
 	public ArrayList<Pair> getLegalMoves(Piece p, Board b) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Pair> legalMoves = new ArrayList<>();
+		int tempX, tempY;
+		
+		//Forward(left)
+		tempX = p.getX() + (2 * p.getDirection());
+		tempY = p.getY() + p.getDirection();
+		if (b.isWithinBounds(tempX, tempY) && b.isCapturable(p, tempX, tempY) && (b.isCellEmpty(tempX, tempY))) {
+			legalMoves.add(new Pair(tempX, tempY));
+		}
+		
+		//Forward(right)
+		tempX = p.getX() + (2 * p.getDirection());
+		tempY = p.getY() - p.getDirection();
+		if (b.isWithinBounds(tempX, tempY) && b.isCapturable(p, tempX, tempY)) {
+			legalMoves.add(new Pair(tempX, tempY));
+		}
+		
+		//Backward(left)
+		tempX = p.getX() - (2 * p.getDirection());
+		tempY = p.getY() + p.getDirection();
+		if (b.isWithinBounds(tempX, tempY) && b.isCapturable(p, tempX, tempY)) {
+			legalMoves.add(new Pair(tempX, tempY));
+		}
+				
+		//Forward(right)
+		tempX = p.getX() - (2 * p.getDirection());
+		tempY = p.getY() - p.getDirection();
+		if (b.isWithinBounds(tempX, tempY) && b.isCapturable(p, tempX, tempY)) {
+			legalMoves.add(new Pair(tempX, tempY));
+		}
+		
+		//Left(Up)
+		tempX = p.getX() -  p.getDirection();
+		tempY = p.getY() - (2 * p.getDirection());
+		if (b.isWithinBounds(tempX, tempY) && b.isCapturable(p, tempX, tempY)) {
+			legalMoves.add(new Pair(tempX, tempY));
+		}
+		
+		//Left(Down)
+		tempX = p.getX() +  p.getDirection();
+		tempY = p.getY() - (2 * p.getDirection());
+		if (b.isWithinBounds(tempX, tempY) && b.isCapturable(p, tempX, tempY)) {
+			legalMoves.add(new Pair(tempX, tempY));
+		}
+		
+		//Right(Up)
+		tempX = p.getX() - p.getDirection();
+		tempY = p.getY() + (2 * p.getDirection());
+		if (b.isWithinBounds(tempX, tempY) && b.isCapturable(p, tempX, tempY)) {
+			legalMoves.add(new Pair(tempX, tempY));
+		}
+				
+		//Right(Down)
+		tempX = p.getX() + (p.getDirection());
+		tempY = p.getY() + (2 * p.getDirection());
+		if (b.isWithinBounds(tempX, tempY) && b.isCapturable(p, tempX, tempY)) {
+			legalMoves.add(new Pair(tempX, tempY));
+		}
+		/*
+		tempX = p.getX() + p.getDirection();
+		tempY = p.getY() - p.getDirection();
+		if (b.isWithinBounds(tempX, tempY) && b.isCapturable(p, tempX, tempY)) {
+			legalMoves.add(new Pair(tempX, tempY));
+		}
+		*/
+
+		return legalMoves;
 	}
+
 }
